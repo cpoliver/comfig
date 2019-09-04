2a79ca70f77aecc5241867e71fadf5cf2ac34a0e
Charles Oliver <charles@cocoa.consulting>
2019-09-04 01:05:10 +0100

    add install script files

---
+++ common/install.sh
@@ -0,0 +1,1 @@
+# TODO: generate or copy ssh key
---
+++ mac/install.sh
@@ -0,0 +1,6 @@
+# TODO: install brew packages
+# TODO: install cask packages
+# TODO: install mac app store apps
+# TODO: install npm packages
+# TODO: copy common config
+# TODO: copy mac-specific config
