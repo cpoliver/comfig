a471fd891c23d040ca65be35c5034b201b8e5b69
Charles P Oliver <charles@cocoa.consulting>
2019-09-06 10:29:48 +0100

    Update install.sh

--- linux/debian/install.sh
+++ linux/debian/install.sh
@@ -1,5 +1,5 @@
-# Typora markdown editor
-wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+# Typora markdown editor# or run:
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt-get update
 sudo apt-get install typora
