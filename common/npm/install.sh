52c0b586451ee6a432bef112756fc51e18d18add
Charles P Oliver <charles@cocoa.consulting>
2021-06-12 12:46:11 +0100

    Update install.sh

--- common/npm/install.sh
+++ common/npm/install.sh
@@ -2,8 +2,32 @@
 
 alias snig="sudo npm i -g"
 
-# node: npm packages
+# npm global utils
 snig commitizen
 snig diff-so-fancy
 snig google-font-installer
 snig svgo
+
+# google fonts
+gfi install "inter"
+gfi install "work sans"
+gfi install "space grotesk"
+gfi install "space mono"
+gfi install "fira sans"
+gfi install "fira sans condensed"
+gfi install "fira sans extra condensed"
+gfi install "fira code"
+gfi install "fira mono"
+gfi install "open sans"
+gfi install "open sans condensed"
+gfi install "rubik"
+gfi install "ibm plex mono"
+gfi install "ibm plex sans"
+gfi install "ibm plex sans condensed"
+gfi install "ibm plex serif"
+gfi install "montserrat"
+gfi install "merriweather"
+gfi install "alegreya"
+gfi install "alegreya sans"
+gfi install "josefin sans"
+gfi install "josefin slab"
