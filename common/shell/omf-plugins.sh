94a076bfa2d3fdc2838f2fcbbe60c33b9f034104
Charles P Oliver <cpoliver@users.noreply.github.com>
2021-07-03 10:43:43 +0100

    Update omf-plugins.sh

--- common/shell/omf-plugins.sh
+++ common/shell/omf-plugins.sh
@@ -1,4 +1,8 @@
-omf install autojump
+omf install bang-bang
+omf install brew
+omf install fasd
 omf install nvm
+omf install osx
+omf install pbcopy
 omf install sudope
-omf install z
+omf install thefuck
